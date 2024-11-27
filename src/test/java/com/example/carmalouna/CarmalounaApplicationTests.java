a4407530d75b04beffc06bdf24191d1dfd833ebc
Markus Hadick <116264057+Markus799@users.noreply.github.com>
2024-11-27 14:25:36 +0100

    setup: project

---
+++ src/test/java/com/example/carmalouna/CarmalounaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carmalouna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarmalounaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
